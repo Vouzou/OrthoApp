48cdecb00fbe6736ae2c9d0609b88735ada4ce6a
Vouzou <Georgios.Vouzounaras@gmail.com>
2016-12-21 16:01:55 -0800

    Add files via upload

--- models/models.js
+++ models/models.js
@@ -1,37 +1,46 @@
 var mongoose = require('mongoose');
  
 var userSchema = mongoose.Schema({
-                username: String,
-                password: String,
-                dropboxToken: String,
-                created_at: {type: Date, default: Date.now}
+        username: String,
+        password: String,
+        dropboxToken: String,
+        created_at: {type: Date, default: Date.now}
 });
  
 var patientSchema = new mongoose.Schema({
-                first_name: String,
-                last_name: String,
-                parent_name: String,
-                date_of_birth: Date,
-                phone: String,
-                address: String,
-                email: String,
-                referral: String,
-                reason: String,
-                medical_history: String,
-                medication: String,
-                treatment: String,
-                first_appointment: Date,
-                payment: String,
-                price: String,
-                notes: String,
-                image_url: String
+        user_id: String,
+        first_name: String,
+        last_name: String,
+        parent_name: String,
+        date_of_birth: Date,
+        phone: String,
+        address: String,
+        email: String,
+        referral: String,
+        reason: String,
+        medical_history: String,
+        medication: String,
+        treatment: String,
+        first_appointment: Date,
+        payment: String,
+        price: String,
+        notes: String,
+        image_url: String
 });
  
 var appointmentSchema = new mongoose.Schema({
-                patient_name: String,
-                date_time: Date
+        user_id: String,
+        patient_name: String,
+        date_time: Date
 });
+
+var dropboxSchema = new mongoose.Schema({
+        user_id: String,
+        token: String
+});
+
  
 mongoose.model("User", userSchema);
 mongoose.model("Patient", patientSchema);
 mongoose.model("Appointment", appointmentSchema);
+mongoose.model("Dropbox", dropboxSchema);
